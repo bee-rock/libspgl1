108194cc82dc4912dbef23c50b9e896ad3379811
Brock <brock.hargreaves@gmail.com>
2015-05-10 18:19:32 -0600

    Adding test for weights.

--- test/test_minimal.cpp
+++ test/test_minimal.cpp
@@ -10,14 +10,41 @@ struct test_minimal : unittest::testcase<> {
     static void run()
     {
         UNITTEST_CLASS(test_minimal)
-        UNITTEST_RUN(test_NormL1_primal)
+        UNITTEST_RUN(test_NormL1_primal_with_weighting_equal_to_one)
+        UNITTEST_RUN(test_NormL1_primal_with_weights_less_than_one)
+        UNITTEST_RUN(test_NormL1_primal_weights_cannot_be_negative)
+        UNITTEST_RUN(test_NormL1_primal_weights_should_be_less_than_or_equal_to_one)
     }
 
-    void test_NormL1_primal()
+    void test_NormL1_primal_with_weighting_equal_to_one()
     {
     	arma::vec v = arma::linspace<arma::vec>(-1, 1, 2);
-    	arma::vec ones = arma::ones<arma::vec>(2);
-    	assert_equal(2.0, libspgl1::NormL1_primal(v, ones));
+    	arma::vec weights = arma::ones<arma::vec>(2);
+    	assert_equal(2.0, libspgl1::NormL1_primal(v, weights));
+    }
+
+    void test_NormL1_primal_with_weights_less_than_one()
+    {
+    	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
+    	arma::vec weights = arma::colvec(2);
+    	weights.fill(0.5);
+    	assert_equal(2.0, libspgl1::NormL1_primal(v, weights));
+    }
+
+    void test_NormL1_primal_weights_cannot_be_negative()
+    {
+    	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
+    	arma::vec weights = arma::colvec(2);
+    	weights.fill(-0.5);
+    	assert_throw<std::invalid_argument>([&v, &weights](){ libspgl1::NormL1_primal(v, weights); }, SPOT);
+    }
+
+    void test_NormL1_primal_weights_should_be_less_than_or_equal_to_one()
+    {
+    	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
+    	arma::vec weights = arma::colvec(2);
+    	weights.fill(1.5);
+    	assert_throw<std::invalid_argument>([&v, &weights](){ libspgl1::NormL1_primal(v, weights); }, SPOT);
     }
 };
 REGISTER(test_minimal);
