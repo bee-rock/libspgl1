ac93a0622d7ccdf16a78fcb1fc9c7d40fe81ae66
Brock <brock.hargreaves@gmail.com>
2015-11-05 23:02:01 -0700

    Some updating, using the latest libunittest.

--- test/test_methods.cpp
+++ test/test_methods.cpp
@@ -49,8 +49,6 @@ struct test_minimal : unittest::testcase<> {
         UNITTEST_RUN(test_set_element)
         UNITTEST_RUN(test_elementwise_multiplication)
         UNITTEST_RUN(test_max)
-        //UNITTEST_RUN(test_NormL1_primal_weights_cannot_be_negative)
-        //UNITTEST_RUN(test_NormL1_primal_weights_should_be_less_than_or_equal_to_one)
     }
 
     void test_dot_product()
@@ -59,7 +57,7 @@ struct test_minimal : unittest::testcase<> {
     	arma::mat row_vector2(2,1);
     	row_vector1.fill(1.0);
     	row_vector2 = row_vector1;
-    	assert_equal(2.0, libspgl1::vector::dot<double>(row_vector1, row_vector2));
+    	ASSERT_EQUAL(2.0, libspgl1::vector::dot<double>(row_vector1, row_vector2));
     }
 
     void test_abs()
@@ -68,7 +66,7 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec result = libspgl1::vector::abs<arma::vec>(vector1);
     	arma::vec expected{1.0,1.0};
     	for (size_t i=0;i<libspgl1::vector::n_elem(vector1);i++){
-    		assert_equal(libspgl1::vector::get_element<double>(expected, i),
+    		ASSERT_EQUAL(libspgl1::vector::get_element<double>(expected, i),
     				     libspgl1::vector::get_element<double>(result, i));
     	}
     }
@@ -79,7 +77,7 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec result = libspgl1::vector::sign<arma::vec>(vector1);
     	arma::vec expected{1.0,-1.0};
     	for (size_t i=0;i<libspgl1::vector::n_elem(vector1);i++){
-    		assert_equal(libspgl1::vector::get_element<double>(expected, i),
+    		ASSERT_EQUAL(libspgl1::vector::get_element<double>(expected, i),
     				     libspgl1::vector::get_element<double>(result, i));
     	}
     }
@@ -91,7 +89,7 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec result = libspgl1::vector::elementwise_multiplication<arma::vec>(vector1, vector2);
     	arma::vec expected{4.0,-6.0};
     	for (size_t i=0;i<libspgl1::vector::n_elem(vector1);i++){
-    		assert_equal(libspgl1::vector::get_element<double>(expected, i),
+    		ASSERT_EQUAL(libspgl1::vector::get_element<double>(expected, i),
     				     libspgl1::vector::get_element<double>(result, i));
     	}
     }
@@ -99,7 +97,7 @@ struct test_minimal : unittest::testcase<> {
     void test_get_element()
     {
     	arma::vec vector1{1.0};
-    	assert_equal(vector1(0), libspgl1::vector::get_element<double>(vector1,0));
+    	ASSERT_EQUAL(vector1(0), libspgl1::vector::get_element<double>(vector1,0));
     }
 
     void test_set_element()
@@ -108,34 +106,34 @@ struct test_minimal : unittest::testcase<> {
     	size_t element_index = 1;
     	double expected_new_element = 10.0;
     	libspgl1::vector::set_element(vector1, element_index, expected_new_element);
-    	assert_equal(expected_new_element, libspgl1::vector::get_element<double>(vector1, element_index));
+    	ASSERT_EQUAL(expected_new_element, libspgl1::vector::get_element<double>(vector1, element_index));
     }
 
     void test_norm_l1()
     {
     	arma::vec a = {-1,1};
-    	assert_equal(2.0, libspgl1::math::norm<double>(a, 1));
+    	ASSERT_EQUAL(2.0, libspgl1::math::norm<double>(a, 1));
     }
 
     void test_max()
     {
     	arma::vec a1 = {-1,1};
     	arma::vec a2 = {-100, 0, 50};
-    	assert_equal(1.0, libspgl1::math::max<double>(a1));
-    	assert_equal(50.0, libspgl1::math::max<double>(a2));
+    	ASSERT_EQUAL(1.0, libspgl1::math::max<double>(a1));
+    	ASSERT_EQUAL(50.0, libspgl1::math::max<double>(a2));
     }
 
     void test_norm_l2()
     {
     	arma::vec a = {3,4};
-    	assert_equal(5.0, libspgl1::math::norm<double>(a, 2));
+    	ASSERT_EQUAL(5.0, libspgl1::math::norm<double>(a, 2));
     }
 
     void test_NormL1_primal_with_weighting_equal_to_one()
     {
     	arma::vec v = arma::linspace<arma::vec>(-1, 1, 2);
     	arma::vec weights = arma::ones<arma::vec>(2);
-    	assert_equal(2.0, libspgl1::math::NormL1_primal<double>(v, weights));
+    	ASSERT_EQUAL(2.0, libspgl1::math::NormL1_primal<double>(v, weights));
     }
 
     void test_NormL1_primal_with_weights_less_than_one()
@@ -143,23 +141,7 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
     	arma::vec weights = arma::colvec(2);
     	weights.fill(0.5);
-    	assert_equal(2.0, libspgl1::math::NormL1_primal<double>(v, weights));
-    }
-
-    void test_NormL1_primal_weights_cannot_be_negative()
-    {
-    	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
-    	arma::vec weights = arma::colvec(2);
-    	weights.fill(-0.5);
-    	assert_throw<std::invalid_argument>([&v, &weights](){ libspgl1::math::NormL1_primal<double>(v, weights); }, SPOT);
-    }
-
-    void test_NormL1_primal_weights_should_be_less_than_or_equal_to_one()
-    {
-    	arma::vec v = arma::linspace<arma::vec>(-2, 2, 2);
-    	arma::vec weights = arma::colvec(2);
-    	weights.fill(1.5);
-    	assert_throw<std::invalid_argument>([&v, &weights](){ libspgl1::math::NormL1_primal<double>(v, weights); }, SPOT);
+    	ASSERT_EQUAL(2.0, libspgl1::math::NormL1_primal<double>(v, weights));
     }
 
     void test_execute_spgl1_method(){
@@ -176,19 +158,18 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec weights = arma::colvec(x.n_rows);
     	weights.ones();
     	arma::vec x_soln = libspgl1::spgl1(A, At, b, x0, weights);
-    	assert_approx_equal_containers(x_soln_from_matlab, x_soln, 1e-10, SPOT);
+    	ASSERT_APPROX_EQUAL_CONTAINERS(x_soln_from_matlab, x_soln, 1e-10);
     }
 
     void test_projectI(){
     	arma::vec x_before_project;
     	arma::vec x_after_project_expected;
-    	x_before_project.load(std::string(DATADIR) + "/x_to_project.csv");
-    	x_after_project_expected.load(std::string(DATADIR) + "/x_after_projection.csv");
+    	bool load__before_project_successful = x_before_project.load(std::string(DATADIR) + "/x_to_project.csv");
+    	bool load_x_after_project_successful = x_after_project_expected.load(std::string(DATADIR) + "/x_after_projection.csv");
     	arma::vec x_after_project = libspgl1::projectI(x_before_project, 100.0);
     	double norm_actual   = libspgl1::math::norm<double>(x_after_project, 1.0);
     	double norm_expected = libspgl1::math::norm<double>(x_after_project_expected, 1.0);
-    	assert_approx_equal(norm_expected, norm_actual, 0.00001, SPOT);
-    	//assert_approx_equal_containers(x_after_project_expected, x_after_project, 0.00001, SPOT);
+    	ASSERT_APPROX_EQUAL(norm_expected, norm_actual, 0.00001);
     }
 
     void test_initialization(){
@@ -200,10 +181,10 @@ struct test_minimal : unittest::testcase<> {
     	arma::vec b = A*x;
     	arma::vec r = libspgl1::initialization::compute_r(A, b, x);
     	double f = libspgl1::initialization::compute_f(r);
-    	assert_equal(0.0, f);
+    	ASSERT_EQUAL(0.0, f);
     	arma::vec g = libspgl1::initialization::compute_g(At, r);
     	for (auto element : g)
-    		assert_equal(0.0, element);
+    		ASSERT_EQUAL(0.0, element);
     }
 
 };
